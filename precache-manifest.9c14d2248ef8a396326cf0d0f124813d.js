f803a34894243a3a2dbe8331067f81bcef857e4d
Lorenze Jay <Lorenzejay@gmail.com>
2020-09-06 09:56:38 -0700

    Updates

---
+++ precache-manifest.9c14d2248ef8a396326cf0d0f124813d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bae05ecb031ef6d08aa677b6ad662750",
+    "url": "/Weather/index.html"
+  },
+  {
+    "revision": "70722c43afb8cbc06ae3",
+    "url": "/Weather/static/css/main.a713b294.chunk.css"
+  },
+  {
+    "revision": "f393acb43b3ddf3d0a33",
+    "url": "/Weather/static/js/2.994ecfd1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather/static/js/2.994ecfd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70722c43afb8cbc06ae3",
+    "url": "/Weather/static/js/main.28b46541.chunk.js"
+  },
+  {
+    "revision": "a2f0f8e0c62843257473",
+    "url": "/Weather/static/js/runtime-main.196e1254.js"
+  },
+  {
+    "revision": "701c10bf0287cba34103f01cf8d29877",
+    "url": "/Weather/static/media/bg.701c10bf.jpg"
+  },
+  {
+    "revision": "8acee2ee2799b21ba37dd0e4cce88de1",
+    "url": "/Weather/static/media/cloudy.8acee2ee.png"
+  },
+  {
+    "revision": "7fc1c14642aadd5639bc2b9c84799674",
+    "url": "/Weather/static/media/moon.7fc1c146.png"
+  },
+  {
+    "revision": "b295288e2db184fb5ed343b8a89195a9",
+    "url": "/Weather/static/media/rain.b295288e.png"
+  },
+  {
+    "revision": "5a69be1aa2348832b7dfed50ac5c7642",
+    "url": "/Weather/static/media/solar.5a69be1a.png"
+  },
+  {
+    "revision": "8f22f87e94672cd78fbe8d6d7d3eb0d9",
+    "url": "/Weather/static/media/sun.8f22f87e.png"
+  },
+  {
+    "revision": "007f286721846c486bb6250deebf34d2",
+    "url": "/Weather/static/media/sunrise.007f2867.png"
+  }
+]);
